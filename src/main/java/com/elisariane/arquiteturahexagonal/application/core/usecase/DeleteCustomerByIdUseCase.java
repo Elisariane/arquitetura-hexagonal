33d448b5561baffef060b92205145246580f0f86
Elisariane <elisarianeBS@gmail.com>
2024-05-13 13:25:07 -0300

    feat: create use case for delete a customer

---
+++ src/main/java/com/elisariane/arquiteturahexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -0,0 +1,23 @@
+package com.elisariane.arquiteturahexagonal.application.core.usecase;
+
+import com.elisariane.arquiteturahexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.elisariane.arquiteturahexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+
+public class DeleteCustomerByIdUseCase {
+
+    private FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    private DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
+
+    public DeleteCustomerByIdUseCase(FindCustomerByIdInputPort findCustomerByIdInputPort, DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
+    }
+
+    public void delete(String id) {
+        findCustomerByIdInputPort.find(id);
+
+        deleteCustomerByIdOutputPort.delete(id);
+    }
+
+}
